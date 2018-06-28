ee1cec16e95f631a5936f2b025bcefdbe39b2911
ben21 <bvanbever1@gmail.com>
2018-06-28 12:10:00 +0200

    added a store observable and an observer component with one working button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 
-
+import { observer } from 'mobx-react';
+import store from './store';
 import './App.css';
 import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+let student1 = {
+  id: 'student1',
+  name: 'Ben'
+}
+let student2 = {
+  id: 'student2',
+  name: 'Nikki'
+}
+let course1 = {
+  id: 'course1',
+  name: 'Math 101'
+}
+
+store.addStudent(student1)
+store.addStudent(student2)
+store.addCourse(course1)
+store.enrollStudent(course1.id, student1.id)
+
+const StudentsList = observer(() => 
+      <div>
+          <h2>{course1.name}</h2>
+          <ul>
+              {store.enrolledStudents(course1.id).map(
+                  n => <li key={n.id}>{n.name}</li>
+              )}
+          </ul>
+          <button
+              onClick={() => store.enrollStudent(course1.id, student2.id)}
+          >Enroll Student 2</button>
+      </div>
+  )
+
+
+
+
 
 const Category = () => (
   <div>
@@ -30,14 +61,14 @@ class App extends React.Component {
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
 
-            <li><Link to="/">Homes</Link></li>
+            <li><Link to="/">Enrollment</Link></li>
             <li><Link to="/category">Category</Link></li>
             <li><Link to="/products">Products</Link></li>
 
           </ul>
          </nav>
 
-           <Route exact path="/" component={Home}/>
+           <Route exact path="/" component={StudentsList}/>
            <Route path="/category" component={Category}/>
            <Route path="/products" component={Products}/>
 
